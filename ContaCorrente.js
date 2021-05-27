b6c41d6344447bd0ef315583764475cf7be4304a
Celso <celsogubeal@gmail.com>
2021-05-27 02:16:31 -0300

    Incluindo get e set encapsulando

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,9 +1,24 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente{
   agencia;
-  cliente;
+  _cliente;
   // #saldo = 0;
   _saldo = 0;
 
+  set cliente(novoValor){
+    if (novoValor instanceof Cliente) {
+      this._cliente = novoValor;
+    }
+  }
+
+  get cliente(){
+    return this._cliente;
+  }
+
+  get saldo(){
+    return this._saldo;
+  }
   sacar(valor){
     if(this._saldo >= valor){
       this._saldo -= valor;
