37333bf8506d030e174dd4a3a404a53317a46333
Celso <celsogubeal@gmail.com>
2021-05-26 21:38:47 -0300

    Incluindo transferencia entre contas

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,20 +1,27 @@
 export class ContaCorrente{
   agencia;
+  cliente;
   // #saldo = 0;
   _saldo = 0;
 
   sacar(valor){
     if(this._saldo >= valor){
       this._saldo -= valor;
+      return valor;
     }
     console.log(this._saldo);
   }
 
   depositar(valor){
-    if(valor > 0){
-      this._saldo += valor;
+    if(valor <= 0){
+      return;
     }
+    this._saldo += valor;
     console.log(this._saldo);
   }
 
+  transferir(valor, cliente) {
+    const valorSacado = this.sacar(valor);
+    cliente.depositar(valorSacado);
+  }
 }
--- index.js
+++ index.js
@@ -10,8 +10,16 @@ cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.sacar(50);
+contaCorrenteRicardo.depositar(500);
+
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
+
+contaCorrenteRicardo.transferir(200, conta2);
+
+console.log(conta2);
 
 console.log(contaCorrenteRicardo);
